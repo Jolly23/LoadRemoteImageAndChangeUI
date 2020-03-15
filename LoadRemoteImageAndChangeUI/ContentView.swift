c3d72264665a406596407520cdadba93c2262007
Jolly23 <zhaolei@pm.me>
2020-03-15 00:05:11 -0700

    Initial Commit

---
+++ LoadRemoteImageAndChangeUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  LoadRemoteImageAndChangeUI
+//
+//  Created by Jolly on 3/15/20.
+//  Copyright © 2020 Jolly. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
